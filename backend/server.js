585882fe044aabaf8848369ade181c6dfd3e19ef
Suvrata Dwivedi <=suvratadwivedi@gmail.com>
2025-06-01 15:52:55 +0530

    server backend configuration

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend/dist/frontend")));
+app.use(express.static(path.join(__dirname, "../frontend/dist/frontend/browser")));
 
 // API Routes
 app.use('/api', routes);
